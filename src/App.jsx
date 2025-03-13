f9d44da03af0a699c12ab184b925193890bf65ea
Muhammad Izhan Waheed <izhan.waheed12@gmail.com>
2025-03-13 17:44:17 +0500

    Integrated Vercel Analytics and updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { PartyPopper,ArrowBigUp, Star } from "lucide-react";
 import "./App.css";
 import MovieList from "./components/MovieList/MovieList";
 import Navbar from "./components/Navbar/Navbar";
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <MovieList type="popular" title="Popular" emoji={<PartyPopper/>} />
       <MovieList type="top_rated" title="Top Rated" emoji={<ArrowBigUp/>} />
       <MovieList type="upcoming" title="Upcoming" emoji={<Star/>} />
+      <Analytics />
     </>
   );
 }
